2ee56ec14b68573b00e33117ca781b2acdb005db
Guido-95 <guidocarna1995@gmail.com>
2021-05-05 17:58:36 +0200

    sistemazione esercizio

--- js/script.js
+++ js/script.js
@@ -18,22 +18,30 @@ function controlloNumeroInArray (elemento,array){
     for(var i=0; i < array.length; i++){
         if(elemento == array[i]){
             return true;
-        }
-        
+        }   
     }
     return false;
 }
 
-// numero random
+// numero random con min e max
 function numeroRandom(min, max) { 
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-
+// scelta della difficoltà, valori: 0 1 2
 var difficolta = parseInt(prompt("scegli una difficoltà tra queste opzioni \n 0 1 2"));
-// variabile difficoltà
+
+// controllo che venga scritto uno dei valori permessi
+while(difficolta != 0 && difficolta != 1 && difficolta != 2 ){
+
+    difficolta= parseInt(prompt("ERRORE, valori permessi \n 0 1 2"));
+
+}
+
+// intervallo della difficoltà, lv1 16 bombe su 100, lv2 16 bombe su 80, lv3 16 bombe su 50;
 var intervalloDifficolta;
-// switch difficoltà
+
+// switch che imposta la difficoltà in base alla scelta
 switch (difficolta) {
     case 0:
         intervalloDifficolta = 100;
@@ -53,21 +61,26 @@ console.log("difficoltà",difficolta);
 
 // variabili
 
+// array vuoto bombe
 var bombe = [];
 
+// massimi tentativi
 var maxTentativi = 84;
 maxTentativi = 5;
 
+// tentativi fatti / punti
 var tentativi = [];
+
 
 var bombaSingola;
 
 // Il computer deve generare 16 numeri casuali tra 1 e 100.
 
 for(i=0; i < 16; i++){
+    // genera 16 bombe in base alla difficoltà
     bombaSingola = numeroRandom(1,intervalloDifficolta);
-    
     console.log("bomba generata: ",bombaSingola, bombe.length);
+    // se il numero bomba non c'è già lo pusha nell'array bombe
     if(!controlloNumeroInArray(bombaSingola,bombe)) {
         
         bombe.push(bombaSingola);
@@ -79,28 +92,38 @@ for(i=0; i < 16; i++){
 console.log(bombe);
 
 // In seguito deve chiedere all'utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+
+
 var numeroUtente;
+
 var haiPerso = false;
 
 for(i=0; i < maxTentativi && haiPerso == false; i++){
-
+ 
+    // chiedi all'utente un numero almeno una volta
     do {
 
         numeroUtente = parseInt(prompt("inserisci un numero fra 1 e " +  intervalloDifficolta));
 
     }
+    // controllo se il numero rispetta le condizioni e non è già presente
     while(controlloNumeroInArray(numeroUtente, tentativi) || numeroUtente > intervalloDifficolta || numeroUtente < 1 || !Number.isInteger(numeroUtente));
-    
+
+    // controllo se il numero corrisponde a un numero bomba
     if(controlloNumeroInArray(numeroUtente, bombe)){
+        // se corrisponde a un numero bomba
         alert("hai perso\n punti: " + tentativi.length );
         haiPerso = true;
     } else {
+        // altrimenti lo pusha nell'array tentativi / punti 
         tentativi.push(numeroUtente);
-}
+    }
 
 }
 
 console.log("tentaivi", tentativi);
-if(!haiPerso){
+
+// se la variabile booleana è false
+if(haiPerso == false){
     alert("COMPLIMENTI, punti" + tentativi.length);
 }
